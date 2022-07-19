c817bdddff7cc5d42b1c6319bda1afcd838a39e4
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-07-19 16:36:51 +0530

    room actions define

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,47 @@
 class RoomsController < ApplicationController
 
-  def index 
+  before_action :set_room, only: %i[edit update destroy]
+
+  def index
+    @rooms = Room.all 
+  end
+
+  def show 
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create 
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:success] = "#{@room.name} is created successfully"
+      redirect_to rooms_path 
+    else 
+      render :new
+    end 
+  end
+
+  def edit 
+  end
+
+  def update 
+    if @room.update(room_params)
+      flash[:success] = "#{@room.name} is updated successfully"
+      redirect_to rooms_path
+    else  
+      render :edit
+    end
+  end
+
+  private 
+
+  def set_room 
+    @room = Room.find(params[:id])
+  end
+
+  def room_params 
+    params.require(:room).permit(:name)
   end
 end
