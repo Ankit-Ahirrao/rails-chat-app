a5b8be2063ee2b7507dcc3b292e680e7f388cd69
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-07-20 16:05:33 +0530

    chat ui done

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,2 +1,14 @@
 class RoomMessagesController < ApplicationController
+
+  before_action :load_attribute
+
+  def create
+    @room_message = RoomMessage.create(user: current_user, room: @room, message: params.dig(:room_message, :message))
+  end
+
+  protected
+
+  def load_attribute 
+    @room = Room.find(params.dig(:room_message, :room_id))
+  end
 end
